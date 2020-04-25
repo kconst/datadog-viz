6389003219811ae1032963e973bb4bdef33dfcbf
Kyle Constance <kconst@gmail.com>
2020-04-25 11:33:34 -0400

    Adding load metric to server response

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
+const os = require('os');
 const http = require("http");
 
 const host = 'localhost';
 const port = 8000;
 
-const reqListener = function (req, res) {
+
+const reqListener = function (req, res) {    
+    const cpus = os.cpus().length;
+    const loadAverage = os.loadavg()[0] / cpus;
     
+    res.setHeader('Content-Type', 'application/json');
     res.writeHead(200);
-    res.end('hello world');
+
+    res.end(JSON.stringify({ loadAverage }));
 };
 
 const server = http.createServer(reqListener);
